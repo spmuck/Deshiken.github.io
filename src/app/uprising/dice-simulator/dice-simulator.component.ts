cbf1e70ed14724e2159619f3de176da4ab4d64c6
Sean Muck <spmuck@gmail.com>
2023-04-05 21:07:40 -0500

    add start of dice simulator

--- src/app/uprising/dice-simulator/dice-simulator.component.ts
+++ src/app/uprising/dice-simulator/dice-simulator.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {FadeInOutAnimation} from "../../shared/animations/fadeInOutAnimation";
 
 @Component({
   selector: 'app-dice-simulator',
   templateUrl: './dice-simulator.component.html',
-  styleUrls: ['./dice-simulator.component.scss']
+  styleUrls: ['./dice-simulator.component.scss'],
+  animations: [
+    FadeInOutAnimation
+  ]
 })
 export class DiceSimulatorComponent {
 
+  diceTower: Array<string> = new Array<string>();
+
+  addDiceToDiceTower(green: string) {
+    this.diceTower.push(green);
+  }
 }
